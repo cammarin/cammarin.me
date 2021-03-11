f426a56c0da5c89a2362276548b05a4983061db5
Carlos Marin <cammarin@gmail.com>
2021-03-11 19:01:28 -0300

    feat(gradient text): apply gradient shadow

--- src/components/GradientText.tsx
+++ src/components/GradientText.tsx
@@ -2,13 +2,17 @@ type GradientTextProps = JSX.IntrinsicElements['span']
 
 const GradientText = ({
   className = '',
+  children,
   ...htmlSpanProps
 }: GradientTextProps) => {
   return (
     <span
-      className={`${className} relative inline-block text-transparent bg-gradient-to-br from-green-400 to-blue-500 bg-clip-text dark:from-yellow-400 dark:to-pink-500 dark:filter-desaturate`}
+      className={`${className} relative inline-block text-transparent bg-gradient-to-br from-green-400 to-blue-500 bg-clip-text shadow-gradient dark:from-yellow-400 dark:to-pink-500 dark:filter-desaturate`}
       {...htmlSpanProps}
-    />
+      data-content={children}
+    >
+      {children}
+    </span>
   )
 }
 
