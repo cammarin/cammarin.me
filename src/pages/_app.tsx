443f03618d81c02a09e1e64eb7e60cb426d2d9b5
Carlos Marin <cammarin@gmail.com>
2021-03-11 12:16:20 -0300

    refactor(tailwind): switch module import to style sheet file
    Needed more flexibility.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import '@/styles/globals.css'
-import 'tailwindcss/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
