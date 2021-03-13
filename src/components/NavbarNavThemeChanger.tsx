0b7e1e119dbe27d7f12af1f21371c11fe9db5eaf
Carlos Marin <cammarin@gmail.com>
2021-03-13 07:26:36 -0300

    refactor(tailwind): remake desaturation filter

--- src/components/NavbarNavThemeChanger.tsx
+++ src/components/NavbarNavThemeChanger.tsx
@@ -22,7 +22,7 @@ const NavbarNavThemeChanger = ({
         {...htmlButtonProps}
       >
         <svg
-          className="w-6 h-6 dark:filter-desaturate"
+          className="w-6 h-6 dark:filter-desaturate-30"
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
           width="24"
--- src/components/Skippy.tsx
+++ src/components/Skippy.tsx
@@ -7,7 +7,7 @@ const Skippy = ({
 }: SkippyProps) => {
   return (
     <a
-      className={`${className} block font-semibold text-light-mode-white text-center bg-gradient-to-br from-green-400 to-blue-500 sr-only focus:outline-none focus:not-sr-only dark:text-dark-mode-black dark:from-yellow-400 dark:to-pink-500 dark:filter-desaturate`}
+      className={`${className} block font-semibold text-light-mode-white text-center bg-gradient-to-br from-green-400 to-blue-500 sr-only focus:outline-none focus:not-sr-only dark:text-dark-mode-black dark:from-yellow-400 dark:to-pink-500 dark:filter-desaturate-30`}
       {...htmlAnchorProps}
     >
       <span className="block py-5 mb-14">{children}</span>
