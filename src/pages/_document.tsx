fa0d9011d02d558f2657681fb30f606509f34886
Carlos Marin <cammarin@gmail.com>
2021-03-13 05:54:37 -0300

    refactor(style): move @apply directives to classes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class Document extends NextDocument {
       <Html>
         <Head />
 
-        <body>
+        <body className="text-neutral-700 tracking-normal bg-neutral-10 dark:text-neutral-30 dark:bg-neutral-900">
           <ColorModeScript />
           <Main />
           <NextScript />
