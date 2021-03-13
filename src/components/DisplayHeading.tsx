0c4674e017b0b417e5ff99d53e5f5b4be0400f02
Carlos Marin <cammarin@gmail.com>
2021-03-13 16:25:03 -0300

    style(tailwind): reorder classes

--- src/components/DisplayHeading.tsx
+++ src/components/DisplayHeading.tsx
@@ -6,7 +6,7 @@ const DisplayHeading = ({
 }: DisplayHeadingProps) => {
   return (
     <h1
-      className={`${className} mb-1 text-5xl sm:text-6xl font-bold tracking-tight`}
+      className={`${className} mb-1 text-5xl font-bold tracking-tight sm:text-6xl`}
       {...htmlHeadingProps}
     />
   )
--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -6,7 +6,7 @@ const SubHeading = ({
 }: SubHeadingProps) => {
   return (
     <p
-      className={`${className} mb-20 text-xl sm:text-2xl text-neutral-400 lowercase tracking-tight dark:text-neutral-80`}
+      className={`${className} mb-20 text-xl text-neutral-400 lowercase tracking-tight dark:text-neutral-80 sm:text-2xl`}
       {...htmlParagraphProps}
     />
   )
