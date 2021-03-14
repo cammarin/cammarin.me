1e48dce7653716570bda1df7b1b3dc0aafc631f1
Carlos Marin <cammarin@gmail.com>
2021-03-14 14:06:38 -0300

    style(skippy): format whitespace

--- src/components/Skippy.tsx
+++ src/components/Skippy.tsx
@@ -10,22 +10,22 @@ const Skippy = ({ children, ...htmlAnchorProps }: SkippyProps) => {
     >
       <span
         className={Utils.minifyString(`
-        relative
-        block
-        p-3
-        font-semibold
-        text-neutral-10
-        text-center
-        bg-gradient-to-br
-        from-green-400
-        to-blue-500
-        glow-md
-        glow-100
-        dark:text-neutral-900
-        dark:from-yellow-400
-        dark:to-pink-500
-        dark:filter-desaturate-30
-      `)}
+          relative
+          block
+          p-3
+          font-semibold
+          text-neutral-10
+          text-center
+          bg-gradient-to-br
+          from-green-400
+          to-blue-500
+          glow-md
+          glow-100
+          dark:text-neutral-900
+          dark:from-yellow-400
+          dark:to-pink-500
+          dark:filter-desaturate-30
+        `)}
       >
         {children}
       </span>
