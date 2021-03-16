4808d8576c5ba9f76b12a037013881c718b860d8
Carlos Marin <cammarin@gmail.com>
2021-03-16 17:42:34 -0300

    feat(responsive): change font size

--- src/components/NavbarNavLink.tsx
+++ src/components/NavbarNavLink.tsx
@@ -17,9 +17,11 @@ const NavbarNavLink = ({
             block
             w-14
             py-2.5
+            text-sm
             font-medium
             text-current
             text-center
+            tracking-wide
             no-underline
             rounded
             hover:text-neutral-800
@@ -34,6 +36,8 @@ const NavbarNavLink = ({
             xs-plus:w-16
             sm-base:w-20
             sm-base:py-3
+            sm-base:text-base
+            sm-base:tracking-normal
           `)}
           {...htmlAnchorProps}
         />
