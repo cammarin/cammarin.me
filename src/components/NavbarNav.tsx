095bd572419360594f580d4e0e862b88f454f4ff
Carlos Marin <cammarin@gmail.com>
2021-04-13 15:11:11 -0300

    refactor(navbar): remake navigation with grid

--- src/components/NavbarNav.tsx
+++ src/components/NavbarNav.tsx
@@ -1,3 +1,4 @@
+import { Children } from 'react'
 import NavbarNavLink from '@/components/NavbarNavLink'
 import NavbarNavThemeChanger from '@/components/NavbarNavThemeChanger'
 import Utils from '@/lib/utils'
@@ -5,9 +6,16 @@ import Utils from '@/lib/utils'
 type NavbarNavProps = JSX.IntrinsicElements['ul']
 
 const NavbarNav = ({ className = '', ...htmlUListProps }: NavbarNavProps) => {
+  const numberOfItems = Children.count(htmlUListProps.children)
+
   return (
     <ul
-      className={Utils.minifyString(`${className} flex`)}
+      className={Utils.minifyString(
+        `${className} grid grid-flow-col gap-0.5 justify-items-end`
+      )}
+      style={{
+        gridTemplateColumns: `repeat(${numberOfItems - 1}, min-content) 1fr`,
+      }}
       {...htmlUListProps}
     />
   )
