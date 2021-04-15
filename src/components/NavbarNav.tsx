a4180deb8783581128912cc4932e939fe9542bad
Carlos Marin <cammarin@gmail.com>
2021-04-14 23:09:53 -0300

    feat(navbar): adjust spacing

--- src/components/NavbarNav.tsx
+++ src/components/NavbarNav.tsx
@@ -15,8 +15,13 @@ const NavbarNav = ({ className = '', ...htmlUListProps }: NavbarNavProps) => {
         auto-cols-min
         grid-flow-col
         gap-x-0.5
+        px-1.5
+        py-1
+        -mx-1.5
         xs-plus:row-start-auto
         xs-plus:col-span-1
+        xs-plus:p-0
+        xs-plus:m-0
       `)}
       {...htmlUListProps}
     />
